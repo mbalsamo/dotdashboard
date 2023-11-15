b176ec24853a61233131f70df286bf2409e23938
Michael Balsamo <mjbalsamo10@gmail.com>
2023-11-15 00:22:48 -0600

    Organized into separate files. Began adding weather GET request

--- code.py
+++ code.py
@@ -8,181 +8,14 @@ import rgbmatrix
 import terminalio
 from digitalio import DigitalInOut, Direction
 from adafruit_ds3231 import adafruit_ds3231
-
-import os
-import ipaddress
-import wifi
-import socketpool
-
 from adafruit_bitmap_font import bitmap_font
-
-wifiSuccess = False
-overwrite = ""
-try:
-    print()
-    print("Attempting Wifi Connection 1")
-    wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
-    print("Connected to WiFi at " + os.getenv('CIRCUITPY_WIFI_SSID'))
-    pool = socketpool.SocketPool(wifi.radio)
-    print("I am now a happy boy")
-    print("My IP address is", wifi.radio.ipv4_address)
-    print()
-    wifiSuccess = True
-    overwrite = "WIFI connected on " + os.getenv('CIRCUITPY_WIFI_SSID')
-    
-except:
-    print("wifi failed first attempt!!!") 
-    overwrite = "WIFI failed to connect. There is no WIFI. RIP"
-    try:
-        print("trying wifi 2")
-        wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID2'), os.getenv('CIRCUITPY_WIFI_PASSWORD2'))
-        print("Connected to WiFi at " + os.getenv('CIRCUITPY_WIFI_SSID2'))
-        pool = socketpool.SocketPool(wifi.radio)
-        print("I am now a happy boy")
-        print("My IP address is", wifi.radio.ipv4_address)
-        overwrite = "WIFI connected on " + os.getenv('CIRCUITPY_WIFI_SSID2')
-        wifiSuccess = True
-    except:
-        print("failed wifi attempt on " + os.getenv('CIRCUITPY_WIFI_SSID2'))
-        overwrite: "Wifi failed both attempts"
+import init
+import views
+import data
 
 
 
-
-bit_depth_value = 5
-base_width = 64
-base_height = 32
-chain_across = 1
-tile_down = 2
-serpentine_value = True
-
-width_value = base_width * chain_across
-height_value = base_height * tile_down
-
-# If there was a display before (protomatter, LCD, or E-paper), release it so
-# we can create ours
-displayio.release_displays()
-
-# send register
-R1 = DigitalInOut(board.GP2)
-G1 = DigitalInOut(board.GP3)
-B1 = DigitalInOut(board.GP4)
-R2 = DigitalInOut(board.GP5)
-G2 = DigitalInOut(board.GP8)
-B2 = DigitalInOut(board.GP9)
-CLK = DigitalInOut(board.GP11)
-STB = DigitalInOut(board.GP12)
-OE = DigitalInOut(board.GP13)
-
-R1.direction = Direction.OUTPUT
-G1.direction = Direction.OUTPUT
-B1.direction = Direction.OUTPUT
-R2.direction = Direction.OUTPUT
-G2.direction = Direction.OUTPUT
-B2.direction = Direction.OUTPUT
-CLK.direction = Direction.OUTPUT
-STB.direction = Direction.OUTPUT
-OE.direction = Direction.OUTPUT
-
-
-
-OE.value = True
-STB.value = False
-CLK.value = False
-
-MaxLed = 64
-
-c12 = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-c13 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-
-for l in range(0, MaxLed):
-    y = l % 16
-    R1.value = False
-    G1.value = False
-    B1.value = False
-    R2.value = False
-    G2.value = False
-    B2.value = False
-
-    if c12[y] == 1:
-        R1.value = True
-        G1.value = True
-        B1.value = True
-        R2.value = True
-        G2.value = True
-        B2.value = True
-    if l > (MaxLed - 12):
-        STB.value = True
-    else:
-        STB.value = False
-    CLK.value = True
-    # time.sleep(0.000002) 
-    CLK.value = False
-STB.value = False
-CLK.value = False
-
-for l in range(0, MaxLed):
-    y = l % 16
-    R1.value = False
-    G1.value = False
-    B1.value = False
-    R2.value = False
-    G2.value = False
-    B2.value = False
-
-    if c13[y] == 1:
-        R1.value = True
-        G1.value = True
-        B1.value = True
-        R2.value = True
-        G2.value = True
-        B2.value = True
-    if l > (MaxLed - 13):
-        STB.value = True
-    else:
-        STB.value = False
-    CLK.value = True
-    # time.sleep(0.000002)
-    CLK.value = False
-STB.value = False
-CLK.value = False
-
-R1.deinit()
-G1.deinit()
-B1.deinit()
-R2.deinit()
-G2.deinit()
-B2.deinit()
-CLK.deinit()
-STB.deinit()
-OE.deinit()
-
-# This next call creates the RGB Matrix object itself. It has the given width
-# and height. bit_depth can range from 1 to 6; higher numbers allow more color
-# shades to be displayed, but increase memory usage and slow down your Python
-# code. If you just want to show primary colors plus black and white, use 1.
-# Otherwise, try 3, 4 and 5 to see which effect you like best.
-#
-matrix = rgbmatrix.RGBMatrix(
-    width=width_value, height=height_value, bit_depth=bit_depth_value,
-    rgb_pins=[board.GP2, board.GP3, board.GP4, board.GP5, board.GP8, board.GP9],
-    addr_pins=[board.GP10, board.GP16, board.GP18, board.GP20],
-    clock_pin=board.GP11, latch_pin=board.GP12, output_enable_pin=board.GP13,
-    tile=tile_down, serpentine=serpentine_value,
-    doublebuffer=True)
-
-
-# Associate the RGB matrix with a Display so that we can use displayio features
-display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
-
-#display.brightness = .1
-display.rotation = 0
-
-# Create two lines of text to scroll. Besides changing the text, you can also
-# customize the color and font (using Adafruit_CircuitPython_Bitmap_Font).
-# To keep this demo simple, we just used the built-in font.
-# The Y coordinates of the two lines were chosen so that they looked good
-# but if you change the font you might find that other values work better.
+display = init.DoTheInitThings()
 
 i2c = busio.I2C(board.GP7, board.GP6)
 rtc = adafruit_ds3231.DS3231(i2c)
@@ -217,21 +50,23 @@ line1 = adafruit_display_text.label.Label(
         text="{}:{:02}{}".format(formatted.tm_hour, formatted.tm_min, pm))
 line1.x = display.width
 line1.y = 5
-line1.scale = 2
+line1.scale = 1
 
 if line1.scale != 1:
     line1.y = line1.y + (line1.scale)
 
+temp, conditions = data.get_weather()
+
 subText = "This is the dot dashboard"
 subTextColor = 0xff8000
-if wifiSuccess == False and overwrite != "":
-    subText = overwrite
-    subTextColor = 0xFF0000
+# if wifiSuccess == False and overwrite != "":
+#     subText = overwrite
+#     subTextColor = 0xFF0000
 
 line2 = adafruit_display_text.label.Label(
     mainfont,
     color=subTextColor,
-        text="It's Saturday")
+        text="{} {}F*".format(conditions, temp))
 line2.x = display.width
 line2.y = 17
 
@@ -251,12 +86,6 @@ g.append(line3)
 display.show(g)
 
 
-def setDateTime():
-    t = time.struct_time((2023, 11, 13, 15, 34, 00, 0, -1, -1))
-    # you must set year, mon, date, hour, min, sec and weekday
-    # yearday is not supported, isdst can be set but we don't do anything with it at this time
-    print(b"Setting time to:", t)  # uncomment for debugging
-    rtc.datetime = t
 
 
 # This function will scoot one label a pixel to the left and send it back to
@@ -288,6 +117,8 @@ WAIT_DURATION_CLOCKUPDATE = 60
 clockUpdateCount = 0
 line3Pause = 7
 
+
+
 while True:
     now = time.monotonic()    
     if now >= LAST_CHANGE_LINE1 + WAIT_DURATION_LINE1 and line1.x != 1:
@@ -311,22 +142,16 @@ while True:
         
     rtc = adafruit_ds3231.DS3231(i2c)
     formatted, pm = GetCurrentTime()
-    line1.text= "{}:{:02}:{:02}".format(formatted.tm_hour, formatted.tm_min, formatted.tm_sec);
+    line1.text= "{}:{:02}{}".format(formatted.tm_hour, formatted.tm_min, pm);
 
     if now >= LAST_CHANGE_CLOCKUPDATE + WAIT_DURATION_CLOCKUPDATE:
         LAST_CHANGE_CLOCKUPDATE = now
         print(now)
-        print("clock update " + "{}:{:02}.{}".format(t.tm_hour, t.tm_min, t.tm_sec))
+        print("clock update " + "{}:{:02}".format(t.tm_hour, t.tm_min, t.tm_sec))
         clockUpdateCount += 1
  
 
         line3.text="Run time: {} min".format(clockUpdateCount)
     
-    # if clockUpdateCount > 5:
-    #     line3Pause = 500
-        
-
-
-    #reverse_scroll(line2)
     display.refresh(minimum_frames_per_second=0)
 
---
+++ init.py
@@ -0,0 +1,187 @@
+import board
+import displayio
+import busio
+import framebufferio
+import time
+import rgbmatrix
+from digitalio import DigitalInOut, Direction
+from adafruit_ds3231 import adafruit_ds3231
+
+import os
+import wifi
+import socketpool
+
+
+def DoTheInitThings():
+    print("Initializing boot")
+    bit_depth_value = 5
+    base_width = 64
+    base_height = 32
+    chain_across = 1
+    tile_down = 2
+    serpentine_value = True
+
+    width_value = base_width * chain_across
+    height_value = base_height * tile_down
+
+    # If there was a display before (protomatter, LCD, or E-paper), release it so
+    # we can create ours
+    displayio.release_displays()
+
+    # send register
+    R1 = DigitalInOut(board.GP2)
+    G1 = DigitalInOut(board.GP3)
+    B1 = DigitalInOut(board.GP4)
+    R2 = DigitalInOut(board.GP5)
+    G2 = DigitalInOut(board.GP8)
+    B2 = DigitalInOut(board.GP9)
+    CLK = DigitalInOut(board.GP11)
+    STB = DigitalInOut(board.GP12)
+    OE = DigitalInOut(board.GP13)
+
+    R1.direction = Direction.OUTPUT
+    G1.direction = Direction.OUTPUT
+    B1.direction = Direction.OUTPUT
+    R2.direction = Direction.OUTPUT
+    G2.direction = Direction.OUTPUT
+    B2.direction = Direction.OUTPUT
+    CLK.direction = Direction.OUTPUT
+    STB.direction = Direction.OUTPUT
+    OE.direction = Direction.OUTPUT
+
+    OE.value = True
+    STB.value = False
+    CLK.value = False
+
+    MaxLed = 64
+
+    c12 = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    c13 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+
+    for l in range(0, MaxLed):
+        y = l % 16
+        R1.value = False
+        G1.value = False
+        B1.value = False
+        R2.value = False
+        G2.value = False
+        B2.value = False
+
+        if c12[y] == 1:
+            R1.value = True
+            G1.value = True
+            B1.value = True
+            R2.value = True
+            G2.value = True
+            B2.value = True
+        if l > (MaxLed - 12):
+            STB.value = True
+        else:
+            STB.value = False
+        CLK.value = True
+        # time.sleep(0.000002) 
+        CLK.value = False
+    STB.value = False
+    CLK.value = False
+
+    for l in range(0, MaxLed):
+        y = l % 16
+        R1.value = False
+        G1.value = False
+        B1.value = False
+        R2.value = False
+        G2.value = False
+        B2.value = False
+
+        if c13[y] == 1:
+            R1.value = True
+            G1.value = True
+            B1.value = True
+            R2.value = True
+            G2.value = True
+            B2.value = True
+        if l > (MaxLed - 13):
+            STB.value = True
+        else:
+            STB.value = False
+        CLK.value = True
+        # time.sleep(0.000002)
+        CLK.value = False
+    STB.value = False
+    CLK.value = False
+
+    R1.deinit()
+    G1.deinit()
+    B1.deinit()
+    R2.deinit()
+    G2.deinit()
+    B2.deinit()
+    CLK.deinit()
+    STB.deinit()
+    OE.deinit()
+
+    # This next call creates the RGB Matrix object itself. It has the given width
+    # and height. bit_depth can range from 1 to 6; higher numbers allow more color
+    # shades to be displayed, but increase memory usage and slow down your Python
+    # code. If you just want to show primary colors plus black and white, use 1.
+    # Otherwise, try 3, 4 and 5 to see which effect you like best.
+    #
+    matrix = rgbmatrix.RGBMatrix(
+        width=width_value, height=height_value, bit_depth=bit_depth_value,
+        rgb_pins=[board.GP2, board.GP3, board.GP4, board.GP5, board.GP8, board.GP9],
+        addr_pins=[board.GP10, board.GP16, board.GP18, board.GP20],
+        clock_pin=board.GP11, latch_pin=board.GP12, output_enable_pin=board.GP13,
+        tile=tile_down, serpentine=serpentine_value,
+        doublebuffer=True)
+
+    # Associate the RGB matrix with a Display so that we can use displayio features
+    display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
+
+    display.rotation = 0
+    return display
+
+
+def InitiateWifiConnection(num):
+    SSID = os.getenv('CIRCUITPY_WIFI_SSID')
+    PASSWORD = os.getenv('CIRCUITPY_WIFI_PASSWORD')
+
+    if num == 2:
+        SSID = os.getenv('CIRCUITPY_WIFI_SSID2')
+        PASSWORD = os.getenv('CIRCUITPY_WIFI_PASSWORD2')
+
+    try:
+        print()
+        print("Attempting Wifi Connection on " + SSID)
+        wifi.radio.connect(SSID, PASSWORD)
+        print("Connected to WiFi")
+        pool = socketpool.SocketPool(wifi.radio)
+        print("I am now a happy boy")
+        print("My IP address is", wifi.radio.ipv4_address)
+        return True
+    except:
+        print("Wifi failed for " + SSID)
+        return False
+
+def AttemptAllWifi():
+    wifiAttempts = 1
+    wifiSuccess = False
+    while wifiSuccess == False and wifiAttempts <= 3:
+        wifiSuccess = InitiateWifiConnection(1)    
+        wifiAttempts += 1
+
+    while wifiSuccess == False and wifiAttempts <= 6:
+        wifiSuccess = InitiateWifiConnection(2)    
+        wifiAttempts += 1
+    print("There were {} wifi attempts".format(wifiAttempts))
+    print()
+    return wifiSuccess
+
+
+def setDateTime():
+    # you must set year, mon, date, hour, min, sec and weekday
+    # yearday is not supported, isdst can be set but we don't do anything with it at this time
+    t = time.struct_time((2023, 11, 13, 15, 34, 00, 0, -1, -1))
+    print(b"Setting time to:", t) 
+    i2c = busio.I2C(board.GP7, board.GP6)
+    rtc = adafruit_ds3231.DS3231(i2c)
+    rtc.datetime = t
